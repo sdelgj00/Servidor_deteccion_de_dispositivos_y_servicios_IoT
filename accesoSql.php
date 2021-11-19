5a0a47b593a251c31e553be9b5c2414a997c2357
sdelgj00 <47608545+sdelgj00@users.noreply.github.com>
2021-11-19 17:27:14 +0100

    Avances en acceso sql
    Ahora se llega a almacenar en la base las diferentes vulnerabilidades de los servicios UPnP

--- accesoSql.php
+++ accesoSql.php
@@ -39,12 +39,76 @@ class accesoSql {
                 $ip=$partesIPPuerto[0];
                 $salida.=$this->anyadirBaseDispositivo($ip);
                 $salida.=$this->anyadirApp($ip,$atr);
-                $idApp = $this->sql("SELECT MAX(ID) AS ID FROM app");
+                //Obtenemos el ID de la app añadida/modificada en el anyadirApp()
+                $idApp = $this->sql("SELECT * FROM app WHERE IP = '".$ip."' AND Port = '".$atr["port"]."'");
                 $salida.=$this->anyadirAppUPnP($idApp[0]["ID"],$atr);
+
+                //Obtenemos el ID de la app añadida/modificada en el anyadirAppUPnP()
+                $idAppUPnP = $this->sql("SELECT * FROM app_UPnP WHERE ID_APP = '".$idApp[0]["ID"]."'");
+                $salida.=$this->anyadirServiciosUPnP($idAppUPnP[0]["ID"],$atr["services"]);
+
+
             }
         }
         if($a["mDNS"]){
             $salida=$salida."hay mDNS\n";
+        }
+        return $salida;
+    }
+    function anyadirServiciosUPnP($idAppUPnP, $atr)
+    {
+        $salida="";
+        foreach ($atr as $serviceName => $serviceAtr) {
+            $res=$this->sql("SELECT * FROM service_UPnP WHERE ID_APP = '".$idAppUPnP."' AND Name= '".$serviceName."'");
+            if(count($res)>0){
+                $salida.="SERVICIO ".$serviceName." ya añadido\n";
+                $res2=$this->sql("UPDATE service_UPnP SET ID_Name= '".$serviceAtr["ID"]."', SCPD= '".$serviceAtr["SCPD"]."', 
+                ControlUrl= '".$serviceAtr["ControlUrl"]."', EventUrl= '".$serviceAtr["EventUrl"]."', BaseUrl= '".$serviceAtr["BaseUrl"]."' 
+                WHERE ID_APP = '".$idAppUPnP."' AND Name= '".$serviceName."'");
+            }else{
+                $salida.= "SERVICIO ".$serviceName." no añadido\n";
+                $res2=$this->sql("INSERT INTO service_UPnP (Name, ID_Name, SCPD, ControlUrl, EventUrl, BaseUrl, ID_APP) values
+                ('".$serviceName."','".$serviceAtr["ID"]."','".$serviceAtr["SCPD"]."','".$serviceAtr["ControlUrl"]."','".$serviceAtr["EventUrl"]."',
+                '".$serviceAtr["BaseUrl"]."','".$idAppUPnP."')");
+            }
+            $idServicio=$this->sql("SELECT * FROM service_UPnP WHERE ID_APP = '".$idAppUPnP."' AND Name= '".$serviceName."'");
+            $salida.=$this->anyadirVulnerabilidadesUPnP($idServicio[0]["ID"],$serviceAtr["vulnerabilities"]);
+        }
+        return $salida;
+    }
+
+    function anyadirVulnerabilidadesUPnP($idServicio, $vulnerabilities){
+        $salida="";
+        if($vulnerabilities["resultsPerPage"]==0) {
+            $salida .= "No hay vulnerabilidades\n";
+            $res = $this->sql("DELETE FROM vulnerability_UPnP WHERE ID_SERVICE= '" . $idServicio . "'");
+        }else{
+            $salida.="hay ".$vulnerabilities["resultsPerPage"]." vulnerabilidades\n";
+            $res=$this->sql("DELETE FROM vulnerability_UPnP WHERE ID_SERVICE= '".$idServicio."'");
+            foreach($vulnerabilities["result"]["CVE_Items"] as $number => $vul){
+                $salida.="vulnerabilidad ".$number."\n";
+                $impactv2=$vul["impact"]["baseMetricV2"];
+                $impactv3=$vul["impact"]["baseMetricV3"];
+                $consulta="INSERT INTO vulnerability_UPnP (ID_SERVICE, PublishDate, LastModifiedDate, Description, Lang,bmv2severity,bmv2explitabilityScore,
+	            bmv2impactScore,bmv2acInsufInfo,bmv2obtainAllPrivilege,	bmv2obtainUserPrivilege,bmv2obtainOtherPrivilege,bmv2userInteractionRequired,bmv2accessVector,
+                bmv2accessComplexity,bmv2authentication,bmv2confidentialityImpact,bmv2integrityImpact,bmv2availabilityImpact,bmv2baseScore,
+                bmv3exploitabilityScore,bmv3impactScore,bmv3attackVector,bmv3attackComplexity,bmv3privilegesRequired,bmv3userInteraction,bmv3scope,bmv3confidentialImpact,
+                bmv3integrityImpact,bmv3availabilityImpact,bmv3baseScore,bmv3baseSeverity) values
+                ('".$idServicio."','".$vul["publishedDate"]."','".$vul["lastModifiedDate"]."','".str_replace("'","`",$vul["cve"]["description"]["description_data"][0]["value"])."',
+                '".$vul["cve"]["description"]["description_data"][0]["lang"]."','".$impactv2["severity"]."','".$impactv2["exploitabilityScore"]."',
+                '".$impactv2["impactScore"]."','".$impactv2["acInsufInfo"]."','".$impactv2["obtainAllPrivilege"]."','".$impactv2["obtainUserPrivilege"]."','".$impactv2["obtainOtherPrivilege"]."','".$impactv2["userInteractionRequired"]."',
+                '".$impactv2["cvssV2"]["accessVector"]."','".$impactv2["cvssV2"]["accessComplexity"]."','".$impactv2["cvssV2"]["authentication"]."','".$impactv2["cvssV2"]["confidentialityImpact"]."',
+                '".$impactv2["cvssV2"]["integrityImpact"]."','".$impactv2["cvssV2"]["availabilityImpact"]."','".$impactv2["cvssV2"]["baseScore"]."',
+                '".$impactv3["exploitabilityScore"]."','".$impactv3["impactScore"]."','".$impactv3["cvssV3"]["attackVector"]."','".$impactv3["cvssV3"]["attackComplexity"]."',
+                '".$impactv3["cvssV3"]["privilegesRequired"]."','".$impactv3["cvssV3"]["userInteraction"]."','".$impactv3["cvssV3"]["scope"]."','".$impactv3["cvssV3"]["confidentialityImpact"]."',
+                '".$impactv3["cvssV3"]["integrityImpact"]."','".$impactv3["cvssV3"]["availabilityImpact"]."','".$impactv3["cvssV3"]["baseScore"]."','".$impactv3["cvssV3"]["baseSeverity"]."')";
+                $res2=$this->sql($consulta);
+                $salida.=$res2."\n";
+                //este if es por si hay algún fallo en la consulta
+                if($res2==""){
+                    $salida.=$consulta."\n";
+                }
+            }
         }
         return $salida;
     }
