15f436ad66b47db4976f63f21b93b0236d1c0ac3
sdelgj00 <47608545+sdelgj00@users.noreply.github.com>
2022-01-21 19:32:19 +0100

    Mejora de la interfaz - Bootstrap
    Falta cambiar colores determinadas tablas, y añadir algún icono por las páginas

--- Dispositivo.php
+++ Dispositivo.php
@@ -2,26 +2,72 @@
  
 <head>
     <title>Dispositivo</title>  
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link href="estilo.css" rel="stylesheet" type="text/css" />
+
+    <script src="jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     <script src="scriptPags.js" type="text/javascript"></script>
     
 </head>
  
 <body>
-<nav><ul>
-        <li><a href="Dispositivos.php">Dispositivos</a></li>
-        <li><a href="#">Protocolos IoT</a>
-            <ul><li><a href="UPnPs.php">UPnP</a></li>
-            <li><a href="mDNSs.php">mDNS</a></li>
-            <li><a href="WS-Discoverys.php">WS-Discovery</a></li>
-            </li>
-    </ul></nav>
-    <h1>Dispositivo <?php echo $_GET['ip'];?></h1>
-    <h2>Puertos</h2>
-	<table>
-		<tr>
+<nav class="navbar navbar-expand-lg navbar-dark bg-info navegador">
+    <title class="navbar-brand">Exploracion IoT</title>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="Dispositivos.php">Ir a inicio</a>
+      </li>
+      <li class="nav-item active dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Protocolos
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="UPnPs.php">UPnP</a>
+          <a class="dropdown-item" href="mDNSs.php">mDNS</a>
+          <a class="dropdown-item" href="WS-Discoverys.php">WS-Discovery</a>
+
+        </div>
+      </li>
+    </ul>
+    <ul class="navbar-nav">
+    <li class="nav-item active dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Recursos
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">             
+            <a class="dropdown-item" href="https://github.com/sdelgj00/Detecci-n-de-dispositivos-y-servicios-IoT"><img src="githubIcon.png" class="icon"></img> Detector IoT</a>
+          <a class="dropdown-item" href="https://github.com/sdelgj00/Servidor_deteccion_de_dispositivos_y_servicios_IoT"><img src="githubIcon.png" class="icon"></img> Servidor</a>
+          <a class="dropdown-item" href="mailto:sdelgj00@estudiantes.unileon.es"><img src="gmail.png" class="icon"></img> Contacto</a>
+        </div>
+      </li>
+    <ul>
+</nav>
+
+    <br>
+    <div class="page-header">
+        <h1 class="h1 titulo">Dispositivo <?php echo $_GET['ip'];?></h1>
+        <hr>
+    </div>
+
+    <br>
+    <h2 class="titulo">Puertos</h2>
+    <hr>
+    <br>
+
+    <table class="text-center table-striped table-responsive-sm table-w contentTable">
+        <tr class="bg-warning flex-column">
 			<th>ID</th><th>Port</th><th>Type</th><th>State</th><th>Name</th><th>Product</th><th>Version</th><th>Extra Info</th><th>Conf</th><th>CPE</th>
 		</tr>
+        <tbody  class='table-dark bg-dark flex-column '>
         <?php
         require("accesoSql.php");
         $accesosql=new accesoSql();
@@ -32,12 +78,18 @@
             </tr>";
         }
     ?>
-	</table>
-    <h2>OS</h2>
-    <table>
-		<tr>
+      </tbody></table>
+
+    <br>
+    <h2 class="titulo">OS</h2>
+    <hr>
+    <br>
+
+    <table class="text-center table-striped table-responsive-sm table-w contentTable">
+        <tr class="bg-warning flex-column">
 			<th>ID</th><th>Name</th><th>Accuracy</th><th>Line</th>
 		</tr>
+        <tbody  class='table-dark bg-dark flex-column '>
         <?php
         $accesosql=new accesoSql();
         $os_script=$accesosql->sql("SELECT * FROM os_script WHERE IP= '".$_GET['ip']."'");
@@ -47,12 +99,18 @@
             </tr>";
         }
     ?>
- 	</table>
-     <h2>UPnP</h2>
-    <table>
-		<tr>
+ 	</tbody></table>
+     
+     <br>
+     <h2 class="titulo">UPnP</h2>
+     <hr>
+     <br>
+
+     <table class="text-center table-striped table-responsive-sm table-w contentTable">
+     <tr class="bg-warning flex-column">
 			<th>ID App</th><th>ID App UPnP</th><th>Port</th><th>Name</th>
 		</tr>
+        <tbody  class='table-dark bg-dark flex-column '>
         <?php
         $accesosql=new accesoSql();
         $apps=$accesosql->sql("SELECT * FROM app WHERE IP= '".$_GET['ip']."'");
@@ -65,12 +123,18 @@
             }
         }
     ?>
- 	</table>
-     <h2>mDNS</h2>
-    <table>
-		<tr>
+ 	</tbody></table>
+          
+     <br>
+     <h2 class="titulo">mDNS</h2>
+     <hr>
+     <br>
+
+     <table class="text-center table-striped table-responsive-sm table-w contentTable">
+     <tr class="bg-warning flex-column">
 			<th>ID App</th><th>ID App mDNS</th>
 		</tr>
+        <tbody  class='table-dark bg-dark flex-column '>
         <?php
         $accesosql=new accesoSql();
         foreach($apps as $i){
@@ -82,12 +146,18 @@
             }
         }
     ?>
- 	</table>
-     <h2>WS-Discovery</h2>
-    <table>
-		<tr>
+ 	</tbody></table>
+             
+     <br>
+     <h2 class="titulo">WS-Discovery</h2>
+     <hr>
+     <br>
+
+     <table class="text-center table-striped table-responsive-sm table-w contentTable">
+     <tr class="bg-warning flex-column">
 			<th>ID app</th><th>ID app WS-Discovery</th>
 		</tr>
+        <tbody  class='table-dark bg-dark flex-column '>
         <?php
         $accesosql=new accesoSql();
         foreach($apps as $i){
@@ -107,7 +177,7 @@
             }
         }
     ?>
- 	</table>
+ 	</tbody></table><br>
 
 </body>
 </html>
--- ServiceUPnP.php
+++ ServiceUPnP.php
@@ -2,31 +2,75 @@
  
 <head>
     <title>Servicio UPnP</title>  
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link href="estilo.css" rel="stylesheet" type="text/css" />
+
+    <script src="jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     <script src="scriptPags.js" type="text/javascript"></script>
     
 </head>
  
 <body>
-    <nav><ul>
-        <li><a href="Dispositivos.php">Dispositivos</a></li>
-        <li><a href="#">Protocolos IoT</a>
-            <ul><li><a href="UPnPs.php">UPnP</a></li>
-            <li><a href="mDNSs.php">mDNS</a></li>
-            <li><a href="WS-Discoverys.php">WS-Discovery</a></li>
-            </li>
-    </ul></nav>
+<nav class="navbar navbar-expand-lg navbar-dark bg-info navegador">
+    <title class="navbar-brand">Exploracion IoT</title>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-    <h1><?php echo str_replace("_"," - ",$_GET["Title"]);?></h1>
-    <h2>Vulnerabilities</h2>
-    <table>
-		<tr>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="Dispositivos.php">Ir a inicio</a>
+      </li>
+      <li class="nav-item active dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Protocolos
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="UPnPs.php">UPnP</a>
+          <a class="dropdown-item" href="mDNSs.php">mDNS</a>
+          <a class="dropdown-item" href="WS-Discoverys.php">WS-Discovery</a>
+
+        </div>
+      </li>
+    </ul>
+    <ul class="navbar-nav">
+    <li class="nav-item active dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Recursos
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">             
+            <a class="dropdown-item" href="https://github.com/sdelgj00/Detecci-n-de-dispositivos-y-servicios-IoT"><img src="githubIcon.png" class="icon"></img> Detector IoT</a>
+          <a class="dropdown-item" href="https://github.com/sdelgj00/Servidor_deteccion_de_dispositivos_y_servicios_IoT"><img src="githubIcon.png" class="icon"></img> Servidor</a>
+          <a class="dropdown-item" href="mailto:sdelgj00@estudiantes.unileon.es"><img src="gmail.png" class="icon"></img> Contacto</a>
+        </div>
+      </li>
+    <ul>
+</nav>
+
+    <br>
+    <div class="page-header">
+        <h1 class="h1 titulo"><?php echo str_replace("_"," - ",$_GET["Title"]);?></h1>
+        <hr>
+    </div>
+    <br>
+    <h2 class="titulo">Vulnerabilities</h2>
+    <hr>
+    <br>
+
+    <table class="text-center table-striped table-responsive-sm table-w contentTable">
+    <tr class="bg-warning flex-column">
 			<th>ID</th><th>Publish Date</th><th>Last Modified Date</th><th>Description</th><th>Lang</th><th>bmv2severity </th><th>bmv2explitabilityScore</th>
             <th>bmv2impactScore</th><th>bmv2acInsufInfo</th><th>bmv2obtainAllPrivilege</th><th>bmv2obtainUserPrivilege</th><th>bmv2obtainOtherPrivilege</th><th>bmv2userInteractionRequired</th>
             <th>bmv2accessVector</th><th>bmv2accessComplexity</th><th>bmv2authentication</th><th>bmv2confidentialityImpact</th><th>bmv2integrityImpact</th><th>bmv2availabilityImpact</th><th>bmv2baseScore</th>
             <th>bmv3exploitabilityScore</th><th>bmv3impactScore</th><th>bmv3attackVector</th><th>bmv3attackComplexity</th><th>bmv3privilegesRequired</th><th>bmv3userInteraction</th><th>bmv3scope</th>
             <th>bmv3confidentialImpact</th><th>bmv3integrityImpact</th><th>bmv3availabilityImpact</th><th>bmv3baseScore</th><th>bmv3baseSeverity</th>
 		</tr>
+        <tbody  class='table-dark bg-dark flex-column '>
         <?php
         require("accesoSql.php");
         $accesosql=new accesoSql();
@@ -49,34 +93,49 @@
             </tr>";
         }
     ?>
-	</table>
+      </tbody></table>
+    <br>
+    
     <?php
     $actions=$accesosql->sql("SELECT * FROM action_upnp WHERE ID_SERVICE= '".$_GET['id']."'");
     foreach($actions as $i){
         echo "<h2>Action ID:".$i["ID"]." Name:".$i["Name"]."</h2>
-        <table>
-		<tr>
-			<th>ID</th><th>Name</th><th>Data Type</th><th>Allowed Value List</th>
-		</tr>";
-        echo "<tr>
+        <hr>
+        <br>
+        <table class='text-center table-striped table-responsive-sm table-w contentTable'>
+        ";
+        echo "<tr class='bg-warning flex-column'>
             <td class=separador1 colspan=4>INPUT ARGS</td>
-        </tr>";
+        </tr>
+        <tr class='bg-warning flex-column'>
+			<th>ID</th><th>Name</th><th>Data Type</th><th>Allowed Value List</th>
+		</tr>
+        <tbody  class='table-dark bg-dark flex-column '>
+        ";
         $inputArgs=$accesosql->sql("SELECT * FROM input_arg_upnp WHERE ID_ACTION= '".$i['ID']."'");
         foreach($inputArgs as $j){
             echo "<tr>
                 <td>".$j["ID"]."</td><td>".$j["Name"]."</td><td>".$j["DataType"]."</td><td>".$j["AllowedValueList"]."</td>
             </tr>";
         }
-        echo "<tr>
+        
+        echo "</tbody></table><br>
+        <table class='text-center table-striped table-responsive-sm table-w contentTable'>
+        <tr class='bg-warning flex-column'>
             <td class=separador1 colspan=4>OUTPUT ARGS</td>
-        </tr>";
+        </tr>
+        <tr class='bg-warning flex-column'>
+			<th>ID</th><th>Name</th><th>Data Type</th><th>Allowed Value List</th>
+		</tr>
+        <tbody  class='table-dark bg-dark flex-column '>
+        ";
         $outputArgs=$accesosql->sql("SELECT * FROM output_arg_upnp WHERE ID_ACTION= '".$i['ID']."'");
         foreach($outputArgs as $j){
             echo "<tr>
                 <td>".$j["ID"]."</td><td>".$j["Name"]."</td><td>".$j["DataType"]."</td><td>".$j["AllowedValueList"]."</td>
             </tr>";
         }
-        echo "</table>";
+        echo "</tbody></table><br>";
     }
     
         
